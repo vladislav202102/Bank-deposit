fde2e7d8a633f443aa3ee697f6d6306214c36515
vladislav202102 <vladikbar@mail.ru>
2021-02-20 21:54:02 +0300

    Main.java added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int deposit = 1000;
+        int integer = 100;
+        int percent = 3;
+        int income = deposit / integer * percent;
+        System.out.println(income);
+    }
+}
