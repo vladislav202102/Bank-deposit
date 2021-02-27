9cf513a852287de210528bcea83736571da803e4
vladislav202102 <vladikbar@mail.ru>
2021-02-27 08:59:20 +0300

    fixed #1, #2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int deposit = 1000;
-        int integer = 100;
-        int percent = 3;
-        int income = deposit / integer * percent;
+        int percent = 100;
+        int cashback = 3;
+        int income = deposit * cashback / percent ;
         System.out.println(income);
     }
 }
